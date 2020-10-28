b1933f52cf63a455fb8a771731cb1b341a51f81f
manjunathgd <manjunath.bchl@gmail.com>
2020-10-28 16:28:03 +0530

    Update:Fix Mojibaki in csv Change-Id: v0.1 Change-Id: I98bbe31ace964f164bd269a3ecb56cf7184ed637 Signed-off-by: manjunathgd <manjunath.bchl@gmail.com>

---
+++ src/com/nichi/model/Constants.java
@@ -0,0 +1,34 @@
+package com.nichi.model;
+
+public class Constants {
+
+	public static final int RESULT_RIGHT_ONLY = 2;
+	public static final int RESULT_LEFT_ONLY = 1;
+	public static final int RESULT_SAME = 0;
+	public static final int RESULT_DIFFERENT = 3;
+	public static final int RESULT_EMPTY = 4;
+	
+	public static final String VERSION = "Version 0.1";
+	
+
+	public static final String APP_ERR_003 = "Java Error : {0}";
+	
+	public static final String HOME_ERR_001 = "Unable to read the selected file at File1!";
+	public static final String HOME_ERR_002 = "Unable to read the selected file at File2!";
+
+	public static final String HOME_WARN_001 = "Please select file path at File1.";
+	public static final String HOME_WARN_002 = "Please select file path at File2.";
+	public static final String HOME_WARN_003 = "Selected path at File1:[{0}] does not exist! Please confirm the path again.";
+	public static final String HOME_WARN_004 = "Selected path at File2:[{0}] does not exist! Please confirm the path again.";
+	
+	public static final String HOME_WARN_005 = "Selected File1 does not seems to be from Alldir.Please check the file fomat.";
+	public static final String HOME_WARN_006 = "Selected File2 does not seems to be from Alldir.Please check the file fomat.";
+
+	public static final String HOME_CONF_001 = "Do you want to compare the selected files?";
+	public static final String RESULT_CONF_002 = "Do you want to export the screen results to a CSV file?";
+	public static final String RESULT_INFO_001 = "The file comparison process is completed.";
+	public static final String RESULT_INFO_002 = "Completed export action and saved to a CSV file.";
+	public static final String DIRECTORY = "Directory";
+	public static final String FILE = "File";
+	
+}
